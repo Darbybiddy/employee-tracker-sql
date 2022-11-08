a9cc0e111828c4b88419073cef7925ab866c19ac
Darby Biddy <darbybiddy3@gmail.com>
2022-11-08 12:48:43 -0600

    created the connections

---
+++ db/connections.js
@@ -0,0 +1,13 @@
+require('dotenv').config()
+const mysql = require('mysql2');
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  // Your MySQL username,
+  user: 'root',
+  // Your MySQL password
+  password: process.env.PW,
+  database: 'employee_tracker_db',
+  
+});
+module.exports = db;
